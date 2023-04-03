290e317897d1e87b1ded51f3097f5c9ec93ed2ca
SamuelBoyer2082251 <2082251@cstj.qc.ca>
2023-04-03 12:17:31 -0400

    Debut page daccueil + ajout des parties du documentaire

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    const transtion_element = document.querySelector('.transition');
+    setTimeout(() => {
+        transtion_element.classList.remove('is-active');
+    }, 500); // millisecondes
+}
