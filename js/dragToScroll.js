518a4941f8dc31372766efae6203514fd3c4aa55
SamuelBoyer2082251 <2082251@cstj.qc.ca>
2023-04-03 17:10:59 -0400

    Changement sur le scroll dans testPage

--- js/dragToScroll.js
+++ js/dragToScroll.js
@@ -1,25 +1,28 @@
 let isDown = false;
-let startX, startY, scrollLeft, scrollTop;
+let startX, scrollLeft;
 
 document.addEventListener('mousedown', (e) => {
-    isDown = true;
-    startX = e.pageX;
-    startY = e.pageY;
-    scrollLeft = document.documentElement.scrollLeft;
-    scrollTop = document.documentElement.scrollTop;
+  isDown = true;
+  startX = e.pageX;
+  scrollLeft = document.documentElement.scrollLeft;
+  scrollRight = document.documentElement.scrollRight;
 });
 
 document.addEventListener('mouseup', () => {
-    isDown = false;
+  isDown = false;
 });
 
 document.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX;
-    const y = e.pageY;
-    const walkX = (x - startX) * 1; // vitesse du scrolling
-    const walkY = (y - startY) * 1;
-    document.documentElement.scrollLeft = scrollLeft - walkX;
-    document.documentElement.scrollTop = scrollTop - walkY;
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX;
+  const walkX = (x - startX) * 1; // vitesse du scrolling pas trop toucher ca bug sinon
+  document.documentElement.scrollLeft = scrollLeft - walkX;
 });
+
+document.addEventListener('wheel', (e) => {
+    const scrollAmount = e.deltaY;
+    document.documentElement.scrollLeft += scrollAmount;
+  });
+
+
